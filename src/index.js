4f7efde40844298a3c90be3c17e3b5da0440800e
yankechao <yankechao@vipkid.com.cn>
2018-07-25 19:44:01 +0800

    安装webpack babel 配置jsx

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from '../lib/index.js'
 console.log(React)
 
 // 描述节点的 对象
-const element = React.createElement('div', {
-  id: 'container',
-  style: 'border: 1px solid red; min-height: 200px'
-},
-  React.createElement('h2', {
-    style: "color: #666"
-  }, 'hello this is a h2',
-    React.createElement('span', { style: 'font-size: 14px' }, 'this is a span in h2')),
-  React.createElement('input'),
-  React.createElement('button', {
-    onclick: () => { alert('clicked') }
-  }, "clickMe"))
+// const element = React.createElement('div', {
+//   id: 'container',
+//   style: 'border: 1px solid red; min-height: 200px'
+// },
+//   React.createElement('h2', {
+//     style: "color: #666"
+//   }, 'hello this is a h2',
+//     React.createElement('span', { style: 'font-size: 14px' }, 'this is a span in h2')),
+//   React.createElement('input'),
+//   React.createElement('button', {
+//     onclick: () => { alert('clicked') }
+//   }, "clickMe"))
+
+const element = (
+  <h1>hello
+    <span>world</span>
+  </h1>
+)
 
 
 React.render(element, document.getElementById('app'))
