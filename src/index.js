f6c0813c91b3b47c3dffccfba5f3bf9b1b52ae0e
yankechao <yankechao@vipkid.com.cn>
2018-07-25 18:57:20 +0800

    add  createElement 方法

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
+const TEXT_ELEMENT = 'TEXT'
+
 // 描述节点的 对象
-const element = {
-  tppe: 'div',
-  props: {
-    id: 'container',
-    children: [
-      { type: 'input', props: { value: 'foo', type: 'text' } },
-      { type: 'a', props: {
-        href: 'www.lingobus.com',
-        children: [{
-          type: 'TEXT',
-          props: { nodeValue: 'baidu' }
-        }]
-      }},
-      { type: 'h3', props: { children: [{
-        type: 'TEXT',
-        props: { nodeValue: '被今天' }
-      }] } },
-      {
-        type: 'span', props: {
-          children: [
-            { type: 'TEXT', props: {nodeValue: 'hello'} }
-          ]
-        }
+const element = createElement('div', {
+  id: 'container',
+  style: 'border: 1px solid red; min-height: 200px'
+},
+  createElement('h2', {
+    style: "color: #666"
+  }, 'hello this is a h2',
+    createElement('span', { style: 'font-size: 14px' }, 'this is a span in h2')),
+  createElement('input'),
+  createElement('button', {
+    onclick: () => { alert('clicked') }
+  }, "clickMe"))
+
+/**
+ * 返回element
+ * @param {*} type
+ * @param {*} props
+ * @param {*} args
+ */
+function createElement(type, configs, ...args) {
+  if (!type) return
+  const props = Object.assign({}, configs)
+  const rawChildren = args.length > 0 ? Array.from(args) : []
+  props.children = rawChildren
+    .filter(c => c != null && c != undefined)
+    .map(c => {
+      if (c instanceof Object) {
+        return c
+      } else if (typeof c === 'string') {
+        return createTextElement(c)
       }
-    ]
+    })
+  return { type, props }
+}
+/**
+ * 创建文字节点
+ * @param {*} str
+ */
+function createTextElement(str) {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      nodeValue: str
+    }
   }
 }
+
+
 
 function render(element, parentDom) {
   const { type, props } = element
 
   //处理 文本节点
-  const dom = type === 'TEXT' ?
+  const dom = type === TEXT_ELEMENT ?
     document.createTextNode('')
     : document.createElement(type)
 
