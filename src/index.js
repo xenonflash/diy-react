ee9ba197a1a8972db054a47cb5d8c51212b34634
yankechao <yankechao@vipkid.com.cn>
2018-07-25 17:43:27 +0800

    add event listener and attribute processing for render function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const element = {
     id: 'container',
     children: [
       { type: 'input', props: { value: 'foo', type: 'text' } },
-      { type: 'input', props: { value: 'foo', type: 'text' } },
-      { type: 'input', props: { value: 'foo', type: 'text' } },
+      { type: 'a', props: { href: 'www.lingobus.com' }},
+      { type: 'h3', props: {  } },
     ]
   }
 }
@@ -14,6 +14,19 @@ const element = {
 function render(element, parentDom) {
   const { type, props } = element
   const dom = document.createElement(type)
+
+  // 事件监听
+  const isEvent = name => /^on/.test(name)
+  Object.keys(props).filter(isEvent).forEach(e => {
+    const eventName = e.toLowerCase().substring(2)
+    dom.addEventListener(eventName, props[e])
+  })
+  // 属性
+  const isAttribute = name => !isEvent(name) && name !== 'children'
+  Object.keys(props).filter(isAttribute).forEach(a => {
+    dom[a] = props[a]
+  })
+  // 递归遍历渲染
   const childElements = props.children || []
   childElements.forEach(child => render(child, dom))
   parentDom.appendChild(dom)
