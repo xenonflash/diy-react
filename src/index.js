9666c888df542cdd5f97468dc877a77f35a9f987
yankechao <yankechao@vipkid.com.cn>
2018-07-26 20:35:15 +0800

    修正reconcile bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from '../lib/index.js'
 
 
-const data = [
+let data = [
   {
     id: 1,
     name: 'this is options a',
@@ -35,15 +35,17 @@ function renderList() {
 }
 function addLike(id) {
   console.log(`addLike`, id)
+  data = data.map(d => {
+    return d.id == id ? Object.assign({}, d, { like: ++d.like }) : d
+  })
+  React.render(app(), document.getElementById('app'))
 }
 
-const element = (
+
+const app = () => (
   <div id="container">
     <h2 style="color: red">hello jsx</h2>
     {renderList()}
   </div>
 )
-
-
-
-React.render(element, document.getElementById('app'))
+React.render(app(), document.getElementById('app'))
